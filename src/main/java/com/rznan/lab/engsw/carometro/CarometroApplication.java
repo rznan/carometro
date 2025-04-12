e3e5c3d4933e0fe6076be4c42e3caae3edc16cb3
Renan <109163037+rznan@users.noreply.github.com>
2025-04-12 17:30:16 -0300

    Commit inicial

---
+++ src/main/java/com/rznan/lab/engsw/carometro/CarometroApplication.java
@@ -0,0 +1,13 @@
+package com.rznan.lab.engsw.carometro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarometroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarometroApplication.class, args);
+	}
+
+}
