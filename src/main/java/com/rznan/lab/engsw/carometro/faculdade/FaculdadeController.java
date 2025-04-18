aeb5c3565f0cff72b487eec27c63b410b74675c0
Renan <109163037+rznan@users.noreply.github.com>
2025-04-18 16:21:06 -0300

    add listagem e adicionar à  Faculdade

--- src/main/java/com/rznan/lab/engsw/carometro/faculdade/FaculdadeController.java
+++ src/main/java/com/rznan/lab/engsw/carometro/faculdade/FaculdadeController.java
@@ -1,16 +1,17 @@
 package com.rznan.lab.engsw.carometro.faculdade;
 
-import com.rznan.lab.engsw.carometro.faculdade.dtos.FaculdadeDto;
+import com.rznan.lab.engsw.carometro.curso.ICursoService;
+import com.rznan.lab.engsw.carometro.faculdade.dtos.CreateFaculdadeDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -18,20 +19,28 @@ import java.util.List;
 public class FaculdadeController {
     @Lazy @Autowired
     private IFaculdadeService faculdadeServiceImpl;
+    @Lazy @Autowired
+    private ICursoService cursoServiceImpl;
 
     @GetMapping
     public String loadListing(Model model) {
-        List<FaculdadeDto> faculdadeDtos = stubGetAllFaculdades();
-        model.addAttribute("faculdades", faculdadeDtos);
+        System.out.println("[Carômetro] -- Carregando página listagem de faculdade");
+        model.addAttribute("faculdades", faculdadeServiceImpl.getAll());
         return "faculdade/listing";
     }
 
-    private static List<FaculdadeDto> stubGetAllFaculdades() {
-        Faculdade f = new Faculdade(1, null, "Teste", Date.from(Instant.now()));
-        List<FaculdadeDto> fs = new ArrayList<>();
-        fs.add(new FaculdadeDto(f));
-        return fs;
+    @GetMapping("/novo")
+    public String loadNewFaculdadePage(Model model) {
+        System.out.println("[Carômetro] -- Carregando página de nova faculdade");
+        model.addAttribute("faculdade", new CreateFaculdadeDto(List.of(), "", LocalDate.now()));
+        model.addAttribute("cursos", cursoServiceImpl.getAll());
+
+        return "faculdade/registry";
     }
 
-
+    @PostMapping
+    public String saveFaculdade(@ModelAttribute CreateFaculdadeDto dto) {
+        faculdadeServiceImpl.save(dto);
+        return "redirect:faculdades";
+    }
 }
