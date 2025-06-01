ac3316fcb4a5b78957fd13d5a094f81842d05fc8
Renan <109163037+rznan@users.noreply.github.com>
2025-06-01 14:15:42 -0300

    cria a home com cards

--- src/main/java/com/rznan/lab/engsw/carometro/home/HomeController.java
+++ src/main/java/com/rznan/lab/engsw/carometro/home/HomeController.java
@@ -1,5 +1,8 @@
 package com.rznan.lab.engsw.carometro.home;
 
+import com.rznan.lab.engsw.carometro.aluno.IAlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+
+    @Autowired
+    IAlunoService alunoServiceImpl;
+
     @RequestMapping("/")
     public String carregarHome(Model model) {
+        model.addAttribute("alunos", alunoServiceImpl.getAll());
         return "home/home";
     }
 }
